d3956a5804249dab7bb5768eeb9e02b2c174c0a7
Irina Rusinova <rusinovarusya@mail.ru>
2021-10-01 11:44:10 +0300

    feat: make count-cats, st-delete-digit, st-encode-line

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  function createNumber(arr) {
+    let num = 0;
+    let length = arr.length;
+    for (let i = 0; i < length; ++i) {
+      num += arr[i] * Math.pow(10, length - i - 1);
+    } return num;
+  }
+
+  let max = Math.floor(n / 10);
+  const digitals = String(n).split('');
+  for (let i = 0; i < digitals.length; ++i) {
+    let tmp = [];
+    digitals.forEach(elem => tmp.push(elem));
+    let d = tmp.splice(i, 1);
+    let newNum = createNumber(tmp);
+    if (newNum > max) max = newNum;
+  } return max;
 }
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let arr = [];
+  let count = 1;
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] != str[i + 1]) {
+      if (count != 1) arr.push(count);
+      arr.push(str[i]);
+      count = 1;
+    }
+    else {
+      count++;
+    }
+  } return arr.join('');
 }
