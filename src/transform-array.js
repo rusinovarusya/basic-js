52aff8b0d91834c4ec1c010c0c7818bfaf56d422
Irina Rusinova <rusinovarusya@mail.ru>
2021-10-03 12:43:08 +0300

    feat: make transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  if (arr.length == 0) return arr;
+  let arr2 = [];
+  for (let i = 0; i < arr.length; ++i) {
+      switch(arr[i]) {
+        case '--discard-next': {
+          i += 2; 
+          break;
+        }
+        case '--discard-prev': {
+          if (arr2.length) arr2.pop(); 
+          break;
+        }
+        case '--double-next': {
+          if (arr[i + 1] !== undefined) {
+            arr2.push(arr[i + 1]); 
+            arr2.push(arr[i + 1]); 
+            i += 1;
+          } 
+            break;
+        }
+        case '--double-prev': {
+          if (arr[i - 1] !== undefined) {
+            arr2.push(arr[i - 1]); 
+          }
+          break;
+        }
+        default: arr2.push(arr[i]);
+      }
+  }
+  return arr2;
 }
